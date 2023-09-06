6db1dc810f2ba80b46e7fdc444bbbbf75da44fde
Lucas Phelipe Castanho Ribeiro <pheliperibeiro2003@gmail.com>
2023-09-06 10:58:30 -0300

    Finish Commit

---
+++ src/AgendaContatos.java
@@ -0,0 +1,62 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int telefone){
+        contatoSet.add(new Contato(nome, telefone));
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatoSet);
+    }
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatoSet) {
+            if (c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarTelefoneContato(String nome, int novoTelefone) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setTelefone(novoTelefone);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContatos();
+
+        agendaContatos.adicionarContato("Camila", 123456);
+        agendaContatos.adicionarContato("Camila", 0);
+        agendaContatos.adicionarContato("Camila Cavalcante", 1111111);
+        agendaContatos.adicionarContato("Camila DIO", 643987);
+        agendaContatos.adicionarContato("Maria Silva", 1111111);
+
+        agendaContatos.exibirContatos();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Camila"));
+
+        System.out.println("Atualizando telefone: " + agendaContatos.atualizarTelefoneContato("Maria Silva", 5555555));
+
+        agendaContatos.exibirContatos();
+
+
+
+    }
+}
